6ec9386115a4755397a7b7f266df6054da8866f5
merchan5801 <merchan5801@gmail.com>
2016-01-08 02:41:00 +0000

    8.7

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,20 @@
 class MessagesController < ApplicationController
+   before_action :set_message, only: [:edit, :update, :destroy]
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
+  end
+  
+  def update
+    if @message.update(message_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+  
   def index
     @message = Message.new
      # Messageを全て取得する。
@@ -22,4 +38,17 @@ class MessagesController < ApplicationController
     params.require(:message).permit(:name, :body)
   end
   ## ここまで
+
+  
+    
 end
+
+
+ 
+
+
+  private
+  def set_message
+    @message = Message.find(params[:id])
+  end
+
