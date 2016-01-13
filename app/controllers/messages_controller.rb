1a89aac7c30d5b3dff0d766ca7b6be0e243a2d27
merchan5801 <merchan5801@gmail.com>
2016-01-13 15:23:31 +0000

    challnege2

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,56 +1,45 @@
 class MessagesController < ApplicationController
-   before_action :set_message, only: [:edit, :update, :destroy]
-  def destroy
-    @message.destroy
-    redirect_to root_path, notice: 'メッセージを削除しました'
-  end
-  
-  def update
-    if @message.update(message_params)
-      # 保存に成功した場合はトップページへリダイレクト
-      redirect_to root_path , notice: 'メッセージを編集しました'
-    else
-      # 保存に失敗した場合は編集画面へ戻す
-      render 'edit'
-    end
-  end
-  
+before_action :set_message, only: [:edit, :update, :destroy]
+
   def index
     @message = Message.new
-     # Messageを全て取得する。
     @messages = Message.all
   end
 
   def create
     @message = Message.new(message_params)
     if @message.save
-      redirect_to root_path , notice: 'メッセージを保存しました'
+      redirect_to root_path , notice: t(:create)
     else
-      # メッセージが保存できなかった時
       @messages = Message.all
-      flash.now[:alert] = "メッセージの保存に失敗しました。"
+      flash.now[:alert] = t(:create_faild)
       render 'index'
     end
   end
-
- private
-  def message_params
-    params.require(:message).permit(:name, :age, :body)
-  end
-  private
- 
-  ## ここまで
-
   
-    
-end
-
-
- 
-
-
-  private
-  def set_message
-    @message = Message.find(params[:id])
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      redirect_to root_path , notice: t(:update)
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: t(:destroy)
   end
 
+  private
+
+    def message_params
+      params.require(:message).permit(:name, :body, :age)
+    end
+  
+    def set_message
+      @message = Message.find(params[:id])
+    end
+end
