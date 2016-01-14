ecf5cf1a90e0613a22c43fdf6268e2bfef84019d
merchan5801 <merchan5801@gmail.com>
2016-01-14 11:02:01 +0000

    challenge2

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,9 +3,10 @@ class Message < ActiveRecord::Base
   # 名前は必須入力かつ20文字以内
   validates :name , length: { maximum: 20 } , presence: true
   # 年齢は1以上99以内
-  validates :age, numericality: { only_integer: true, greater_than: 0, less_than: 100 }, presence: true 
+  validates :age, numericality: { only_integer: true , less_than: 100 }, presence: true 
   # 内容は必須入力かつ2文字以上30文字以下
   validates :body , length: { minimum: 2, maximum: 30 } , presence: true
 end
 
 
+#validates :age, numericality: { only_integer: true, greater_than: 0, less_than: 100 }, presence: true
